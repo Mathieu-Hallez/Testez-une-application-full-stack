c66b4619f3a64d32a228f8102a672dde4f78178a
Mr_mickcraft 45 <mickcraft45@gmail.com>
2024-01-22 11:48:38 +0100

    Write some tests cases

---
+++ back/src/test/java/com/openclassrooms/starterjwt/services/SessionServiceTest.java
@@ -0,0 +1,37 @@
+package com.openclassrooms.starterjwt.services;
+
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mock;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+@SpringBootTest
+public class SessionServiceTest {
+
+    @BeforeEach
+    public void setup() {
+
+    }
+
+    // Create a session
+
+    // Delete a session
+
+    // findAll sessions
+
+    // getBytId a session
+
+    // Update a session
+
+    // Participate to a session
+    // - session not found
+    // - user not found
+    // - user already participate to the session
+    // - successfully participate
+
+    // No longer participate to a session
+    // - session not found
+    // - user no already participate to the session
+    // - successfully leave session
+}
