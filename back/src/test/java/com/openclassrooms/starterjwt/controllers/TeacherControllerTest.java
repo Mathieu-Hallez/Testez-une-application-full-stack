1572e93e7b17a935a82d9eff869a6188c886e805
Mr_mickcraft 45 <mickcraft45@gmail.com>
2024-02-04 21:20:24 +0100

    Create and list tests for User and Teacher Controller

---
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/TeacherControllerTest.java
@@ -0,0 +1,16 @@
+package com.openclassrooms.starterjwt.controllers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Unit Test for Teacher Service")
+public class TeacherControllerTest {
+    // FindById
+    // - OK with check good teacher return
+    // - BadRequest with incorrect id
+
+    // FindAll
+    // - Ok with check list size and teach firstname
+}
---
+++ back/src/test/java/com/openclassrooms/starterjwt/controllers/UserControllerTest.java
@@ -0,0 +1,23 @@
+package com.openclassrooms.starterjwt.controllers;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.junit.jupiter.MockitoExtension;
+
+@ExtendWith(MockitoExtension.class)
+@DisplayName("Unit Test for User Service")
+public class UserControllerTest {
+    // FindById
+    // - Ok check user name
+    // - NotFound
+    // - BadRequest
+
+    // Delete
+    // - Ok
+    // - NotFound
+    // - BadRequest
+    // - Unauthorized
+
+    // Delete (Integration Test)
+    // - Ok and call delete of userService
+}
