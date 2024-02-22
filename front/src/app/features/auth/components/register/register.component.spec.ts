c7adc7c99ea2b7d9166992d83eb9e55b01106714
Mathieu.HALLEZ <mathew.hallez@gmail.com>
2024-02-22 13:08:45 +0100

    Add a test on submit methods of registerComponent

--- front/src/app/features/auth/components/register/register.component.spec.ts
+++ front/src/app/features/auth/components/register/register.component.spec.ts
@@ -1,6 +1,6 @@
 import { HttpClientModule } from '@angular/common/http';
-import { ComponentFixture, TestBed } from '@angular/core/testing';
-import { FormBuilder, ReactiveFormsModule } from '@angular/forms';
+import { ComponentFixture, TestBed, fakeAsync, tick } from '@angular/core/testing';
+import { ReactiveFormsModule } from '@angular/forms';
 import { MatCardModule } from '@angular/material/card';
 import { MatFormFieldModule } from '@angular/material/form-field';
 import { MatIconModule } from '@angular/material/icon';
@@ -9,12 +9,24 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { expect } from '@jest/globals';
 
 import { RegisterComponent } from './register.component';
+import { By } from '@angular/platform-browser';
+import { AuthService } from '../../services/auth.service';
+import { of } from 'rxjs';
+import { Router } from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import { routes } from '../../auth-routing.module';
 
 describe('RegisterComponent', () => {
   let component: RegisterComponent;
   let fixture: ComponentFixture<RegisterComponent>;
+  let authServiceStub : any;
+  let router : Router;
 
   beforeEach(async () => {
+    authServiceStub = {
+      register: jest.fn()
+    };
+
     await TestBed.configureTestingModule({
       declarations: [RegisterComponent],
       imports: [
@@ -24,17 +36,46 @@ describe('RegisterComponent', () => {
         MatCardModule,
         MatFormFieldModule,
         MatIconModule,
-        MatInputModule
+        MatInputModule,
+        RouterTestingModule.withRoutes(routes)
+      ],
+      providers: [
+        {
+          provide: AuthService,
+          useValue: authServiceStub
+        }
       ]
     })
       .compileComponents();
 
+      router = TestBed.inject(Router);
+
     fixture = TestBed.createComponent(RegisterComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
+
+    router.initialNavigation();
   });
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+
+  it('should call form methods and redirect to login page', () => {
+    // Arrange
+    jest.spyOn(component, 'submit');
+    jest.spyOn(authServiceStub, 'register').mockReturnValue(of());
+    const navigateSpy = jest.spyOn(router, 'navigate');
+    const submitForm = fixture.debugElement.query(By.css('.register-form'));
+
+    // Act
+    submitForm.triggerEventHandler('ngSubmit', null);
+    fixture.detectChanges();
+
+    // Assert
+    expect(component.submit).toHaveBeenCalled();
+    expect(authServiceStub.register).toHaveBeenCalled();
+    expect(component.onError).toBeFalsy();
+    // expect(navigateSpy).toHaveBeenCalled();
   });
 });
